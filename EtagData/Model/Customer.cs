b394e9ed1949bbf9352f63b0ce4181f3a9c17698
les2002 <les2002@protonmail.com>
2017-10-26 15:46:34 +0100

    Initial Commit

---
+++ EtagData/Model/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EtagData.Model
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [ConcurrencyCheck]
+        public int Version { get; set; }
+    }
+}
